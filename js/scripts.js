33c4dcb861a9ee0326a4de894f6f7a9f2cc06248
amigdol1 <alex.migdol@gmail.com>
2016-06-08 23:45:44 -0700

    add additional form questions and formatting

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,6 @@ $(document).ready(function() {
 		$var name = $("#name").val();
 		$var email = $("#email").val();
 		$var climate = $("input:radio[name=climate]:checked").val();
+		$var residence = $("input:radio[name=climate]:checked").val();
 	});
 });
