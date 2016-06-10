38ac3e26d227f09abb21ef4c71f8f4169b642f06
amigdol1 <alex.migdol@gmail.com>
2016-06-10 09:57:49 -0700

    fix variable name in js, but will still contain an error in branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,16 @@
-$(document).ready(function() {
-	$('.carousel').carousel({
-  interval: 3000
-})
+alert('hi')
 
+$(document).ready(function() {
 	$("#blanks").submit(function() {
 
-		$var name = $("#name").val();
-		$var email = $("#email").val();
-		$var climate = $("input:radio[name=climate]:checked").val();
-		$var residence = $("input:radio[name=climate]:checked").val();
+		var name = $("#name").val();
+		var email = $("#email").val();
+		var climate = $("input:radio[name=climate]:checked").val();
+		var residence = $("input:radio[name=climate]:checked").val();
 
-		var result;
 
-		if ((climate === "hot") && (residence === "house")) {
-			result === hot_house;
-		} else if ((climate === "cold") && (residence === "house")) {
-			result === cold_house;
-		} else if ((climate === "hot") && (residence === "resort")) {
-			result === hot_resort;
-		} else if ((climate === "cold") && (residence === "resort")) {
-			result === cold_resort;
-		} else if ((climate === "hot") && (residence === "no_preference")) {
-			result === cold_resort;
-			else if ((climate === "cold") && (residence === "no_preference")) {
-			result === cold_resort;
 
-			$(#result).text(results);
+	
+
 	});
 });
