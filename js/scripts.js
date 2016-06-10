69c8002fbd5928935a2df9c78140b11dd45dcb99
amigdol1 <alex.migdol@gmail.com>
2016-06-10 10:22:53 -0700

    fix bug with residence radio button value in js and add console logs of the output of js logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
-alert('hi')
+
 
 $(document).ready(function() {
-	$("#blanks").submit(function() {
-
+	$("#blanks").submit(function(event) {
+		event.preventDefault();
 		var name = $("#name").val();
 		var email = $("#email").val();
 		var climate = $("input:radio[name=climate]:checked").val();
-		var residence = $("input:radio[name=climate]:checked").val();
+		var residence = $("input:radio[name=residence]:checked").val();
 
 
+		var result;
+		if ((climate === "hot") && (residence === "house")) {
+			result = "hot_house";
+		} else if ((climate === "cold") && (residence === "house")) {
+			result = "cold_house";
+		} else if ((climate === "hot") && (residence === "resort")) {
+			result = "hot_resort";
+		} else if ((climate === "cold") && (residence === "resort")) {
+			result = "cold_resort";
+		} else {
+			result = "view_all";
+		}
 
-	
+
+		console.log(result)
+
+			$("result").text(result);
+
 
 	});
 });
