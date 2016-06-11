5aa1302318bacdc74701b9f2cf8393893a1996d8
amigdol1 <alex.migdol@gmail.com>
2016-06-11 15:33:32 -0700

    add a disable state to the submit button to prevent users from resubmission without refreshing.

--- js/scripts.js
+++ js/scripts.js
@@ -8,22 +8,28 @@ $(document).ready(function() {
 		var age = $("input:radio[name=age]:checked").val();
 
 			if ((climate === "hot") && (residence === "house")) {
-				$("#hot_house_text").show();
+				$(".hot_house_text").show();
 			} else if ((climate === "cold") && (residence === "house")) {
-					$("#cold_house_text").show();
+					$(".cold_house_text").show();
 			} else if ((climate === "hot") && (residence === "resort")) {
-					$("#hot_resort_text").show();
+					$(".hot_resort_text").show();
 			} else if ((climate === "cold") && (residence === "resort")) {
-					$("#cold_resort_text").show();
+					$(".cold_resort_text").show();
 			} else if ((climate === "hot") && (residence === "no_preference")) {
-					$("#both_hot_text").show();
+					$(".both_hot_text").show();
 			} else if ((climate === "cold") && (residence === "no_preference")) {
-					$("#both_cold_text").show();
+					$(".both_cold_text").show();
 			} else {
 				alert("Oops! Looks like you didn't answer all of the required fields. Be sure to answer all required questions, so our team can give you the best results.")
 			}
 
 			console.log(name, email, climate, residence, age)
 
+			// disables resubmissions 
+		  $('button#suggester').on('click',function() {
+    	$(this).val('Please wait ...')
+      .attr('disabled','disabled');
+    	$('#suggesterform').submit();
+    });
 	});
 });
