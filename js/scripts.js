ab77b3e321bb43741543cebeb85cc486dac89807
amigdol1 <alex.migdol@gmail.com>
2016-06-11 08:50:17 -0700

    add console log to variables entered in form to gather the information

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,27 @@ $(document).ready(function() {
 		var email = $("#email").val();
 		var climate = $("input:radio[name=climate]:checked").val();
 		var residence = $("input:radio[name=residence]:checked").val();
+		var age = $("input:radio[name=age]:checked").val();
 
+			if ((climate === "hot") && (residence === "house")) {
+				$("#hot_house_text").show();
+			} else if ((climate === "cold") && (residence === "house")) {
+					$("#cold_house_text").show();
+			} else if ((climate === "hot") && (residence === "resort")) {
+					$("#hot_resort_text").show();
+			} else if ((climate === "cold") && (residence === "resort")) {
+					$("#cold_resort_text").show();
+			} else if ((climate === "hot") && (residence === "no_preference")) {
+					$("#both_hot_text").show();
+			} else if ((climate === "cold") && (residence === "no_preference")) {
+					$("#both_cold_text").show();
+			} else {
+				alert("Oops! Looks like you didn't fill in all of the required fields. Be sure to answer all required questions, so our team can give you the best results.")
+			}
 
-		if ((climate === "hot") && (residence === "house")) {
-			$("#hot_house_text").show();
-		} else if ((climate === "cold") && (residence === "house")) {
-				$("#cold_house_text").show();
-		} else if ((climate === "hot") && (residence === "resort")) {
-				$("#hot_resort_text").show();
-		} else if ((climate === "cold") && (residence === "resort")) {
-				$("#cold_resort_text").show();
-		} else if ((climate === "hot") && (residence === "no_preference")) {
-				$("#both_hot_text").show();
-		} else if ((climate === "cold") && (residence === "no_preference")) {
-				$("#both_cold_text").show();
-		} else {
-			alert("Oops! Looks like you didn't fill in all of the required fields. Be sure to answer all required questions, so our team can give you the best results.")
-		}
+			console.log(name, email, climate, residence, age)
 
-
-			
-
+	// add/remove id's for response when form submitting again w/o refresh
 
 	});
 });
